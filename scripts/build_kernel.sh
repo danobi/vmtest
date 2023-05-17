2afeaf82cbd2ecc621af62bec4a6d5e11c205425
Daniel Xu <dxu@dxuuu.xyz>
2023-05-16 23:11:47 -0600

    Build 5.15 LTS kernel
    Seems like 5.15 is the oldest LTS kernel we can build without major
    hassle.

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -10,6 +10,14 @@
 
 set -eu
 
+# Formats a version string to a comparable number
+# eg. v1.2.3 => 1002003
+function version {
+	# Strip the leading 'v'
+	local vstripped="${1:1}"
+	echo "$vstripped" | awk -F. '{ printf("%d%03d%03d\n", $1,$2,$3); }'
+}
+
 # Go to repo root
 cd "$(git rev-parse --show-toplevel)"
 
@@ -24,8 +32,18 @@ DISTRO=${2:-empty}
 # Unique identifier for the kernel being built
 IDENTIFIER="$1"-"$DISTRO"
 
+# Anything older than 5.15 we need to use alpine 3.16, which at the time of
+# writing is the oldest supported alpine version that contains all the build
+# deps (pahole in particular).  We need old alpines to use an older GCC. Newer
+# GCCs have better warnings and they break old kernel builds.
+ALPINE_VERSION=3.18
+if [[ $(version "$1") -le $(version "v5.15") ]]; then
+	ALPINE_VERSION=3.16
+fi
+
 # Build builder
 docker build \
+	--build-arg ALPINE_VERSION="$ALPINE_VERSION" \
 	--build-arg KERNEL_TAG="$1" \
 	--build-arg DISTRO="$DISTRO" \
 	-t vmtest-kernel-builder-"$IDENTIFIER" \

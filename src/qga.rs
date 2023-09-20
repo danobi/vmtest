02a3899f93793ce502f05481a3997a4971380035
Harsh Modi <hmodi@aviatrix.com>
2023-09-20 05:53:47 +0000

    Adjust timeouts and add additional logging

--- src/qga.rs
+++ src/qga.rs
@@ -1,16 +1,14 @@
-
 use std::os::unix::net::UnixStream;
 use std::path::PathBuf;
 
-
-
+use std::thread;
 use std::time::{Duration, Instant};
 
-use anyhow::{anyhow, Context, Result};
-use log::{error, warn};
+use anyhow::{bail, Context, Result};
+use log::{error, info, warn};
 use qapi::{qga, Command as QapiCommand, Qga};
 use rand::Rng;
-use scopeguard::defer;
+
 
 const KVM_TIMEOUT: Duration = Duration::from_secs(30);
 const EMULATE_TIMEOUT: Duration = Duration::from_secs(120);
@@ -41,23 +39,33 @@ impl QgaWrapper {
         // If we try reading the socket too  early, we'll hang forever and never run the test.
         // So do the guest_sync first with a timeout to ensure that the VM Guest Agent is up.
         let end = Instant::now() + timeout;
+        let mut i = 0;
         while Instant::now() < end {
+            info!("Connecting to QGA ({i})");
+            i += 1;
             let qga_stream = match UnixStream::connect(&sock) {
                 Ok(s) => s,
                 Err(e) => {
                     error!("Failed to connect QGA, retrying: {}", e);
+                    thread::sleep(Duration::from_secs(1));
                     continue;
                 }
             };
-            qga_stream.set_read_timeout(Some(Duration::from_secs(1)))?;
+            qga_stream.set_read_timeout(Some(Duration::from_secs(5)))?;
             let mut qga = Qga::from_stream(&qga_stream);
             let sync_value = rand::thread_rng().gen_range(1..10_000);
-            if let Ok(_) = qga.guest_sync(sync_value) {
-                return Ok(Self { stream: qga_stream });
+            match qga.guest_sync(sync_value) {
+                Ok(_) => {
+                    return Ok(Self { stream: qga_stream });
+                }
+                Err(e) => {
+                    warn!("QGA sync failed, retrying: {e}");
+                    thread::sleep(Duration::from_secs(1));
+                }
             }
         }
 
-        Err(anyhow!("Timed out waiting for QGA connection"))
+        bail!("Timed out waiting for QGA connection");
     }
 
     /// Run a command inside the guest
@@ -65,9 +73,6 @@ impl QgaWrapper {
         &self,
         args: qga::guest_exec,
     ) -> Result<<qga::guest_exec as QapiCommand>::Ok> {
-        if let Err(e) = self.stream.set_read_timeout(None) {
-            warn!("Error setting socket timeout: {e}");
-        }
         let mut qga = Qga::from_stream(&self.stream);
         qga.execute(&args).context("Error running guest_exec")
     }
@@ -77,14 +82,6 @@ impl QgaWrapper {
         &self,
         pid: i64,
     ) -> Result<<qga::guest_exec_status as QapiCommand>::Ok> {
-        defer! {
-            if let Err(e) = self.stream.set_read_timeout(None) {
-                warn!("Error setting socket timeout: {e}");
-            }
-        };
-        if let Err(e) = self.stream.set_read_timeout(Some(Duration::from_secs(5))) {
-            warn!("Error setting socket timeout: {e}");
-        }
         let mut qga = Qga::from_stream(&self.stream);
         qga.execute(&qga::guest_exec_status { pid })
             .context("error running guest_exec_status")

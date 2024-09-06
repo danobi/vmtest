76795cce1802a6debcebe3e0eaada997d4c5bd33
Daniel Xu <dxu@dxuuu.xyz>
2024-09-06 14:25:51 -0600

    XXX: build release binaries in emulated environment
    this works for x86-64 and aarch64, but looks like
    s390x-unknown-linux-musl is not supported by rustc at all:
    https://doc.rust-lang.org/nightly/rustc/platform-support.html

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -3,9 +3,9 @@
 # Build a release version of vmtest for a list of architectures.
 # The resulting binaries will be copied in the current directory and named vmtest-<arch>.
 #
-# This script assumes it is run on a Debian based system.
-#
 # Run this on your root host inside vmtest repository.
+#
+# Note: for foreign architectures we will emulate using qemu. This can be slow.
 #
 # Usage:
 #   ./scripts/build_release.sh
@@ -15,28 +15,40 @@ set -eu
 
 ARCHS=(x86_64 aarch64 s390x)
 
+# Allow user to pick subset
 if [[ $# -gt 0 ]]
 then
     ARCHS=("$@")
 fi
 
-# Install the required toolchain for cross-compilation
-X_ARCHS=()
+declare -A ARCH_TO_IMAGE_ARCH=(
+  [x86_64]=amd64
+  [aarch64]=arm64v8
+  [s390x]=s390x
+)
+
+declare -A ARCH_TO_DOCKER_ARCH=(
+  [x86_64]=linux/amd64
+  [aarch64]=linux/arm64
+  [s390x]=linux/s390x
+)
+
+# Install binfmt hooks - very magical (but is necessary)
+docker run --rm --privileged tonistiigi/binfmt --install all
+
 for arch in "${ARCHS[@]}"; do
-    if [[ "${arch}" == "$(uname -m)" ]]; then
-        continue
-    fi
-    X_ARCHS+=("${arch}")
-done
+  image_name="vmtest-release-${arch}"
 
-ARCHS_TO_EXPAND=$(IFS=, ; echo "${X_ARCHS[*]}")
+  docker build \
+    --platform "${ARCH_TO_DOCKER_ARCH[${arch}]}" \
+    --build-arg IMAGE_ARCH="${ARCH_TO_IMAGE_ARCH[$arch]}" \
+    --build-arg TARGET_ARCH="${arch}" \
+    -f scripts/docker/Dockerfile.release \
+    -t "${image_name}" \
+    .
 
-sudo apt update
-eval sudo apt install -y "gcc-{${ARCHS_TO_EXPAND//_/-}}-linux-gnu"
-eval rustup target add "{${ARCHS_TO_EXPAND}}-unknown-linux-gnu"
-
-for arch in "${ARCHS[@]}"; do
-    # Compile the binary
-    RUSTFLAGS="-C target-feature=+crt-static -C linker=/usr/bin/${arch}-linux-gnu-gcc" cargo build --release --target "${arch}-unknown-linux-gnu"
-    cp "./target/${arch}-unknown-linux-gnu/release/vmtest" "./vmtest-${arch}"
+  # Need to create a container to copy from it
+  id=$(docker create "${image_name}")
+  docker cp "${id}:/output/vmtest-${arch}" .
+  docker rm -v "${id}"
 done

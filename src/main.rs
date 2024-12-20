danobi/vmtest

commit ab2595877389f6d533ddede8eee9df70477bf02e

Support oneliner interface for kernel targets
The oneliner interface is an alternative to the `vmtest.toml` config
file. It's nice when you wanna do something one-off and don't wanna both
with creating a new file.

src/main.rs
@@ -1,19 +1,20 @@
+use std::env;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 
 use anyhow::{Context, Result};
 use clap::Parser;
 use regex::Regex;
 
-use ::vmtest::{Ui, Vmtest};
+use ::vmtest::{Config, Target, Ui, Vmtest};
 
 #[derive(Parser, Debug)]
 #[clap(version)]
 struct Args {
     /// Path to config file
-    #[clap(short, long, default_value = "vmtest.toml")]
-    config: PathBuf,
+    #[clap(short, long)]
+    config: Option<PathBuf>,
     /// Filter by regex which targets to run
     ///
     /// This option takes a regular expression. If a target matches this regular
@@ -22,16 +23,44 @@ struct Args {
     /// Supported regex syntax: https://docs.rs/regex/latest/regex/#syntax.
     #[clap(short, long, default_value = ".*")]
     filter: String,
+    #[clap(short, long, conflicts_with = "config")]
+    kernel: Option<PathBuf>,
+    #[clap(conflicts_with = "config")]
+    command: Vec<String>,
+}
+
+/// Configure a `Vmtest` instance from command line arguments.
+fn config(args: &Args) -> Result<Vmtest> {
+    if let Some(kernel) = &args.kernel {
+        let cwd = env::current_dir().context("Failed to get current directory")?;
+        let config = Config {
+            target: vec![Target {
+                name: kernel.file_name().unwrap().to_string_lossy().to_string(),
+                image: None,
+                uefi: false,
+                kernel: Some(kernel.clone()),
+                kernel_args: None,
+                command: args.command.join(" "),
+            }],
+        };
+
+        Vmtest::new(cwd, config)
+    } else {
+        let default = Path::new("vmtest.toml").to_owned();
+        let config_path = args.config.as_ref().unwrap_or(&default);
+        let contents = fs::read_to_string(config_path).context("Failed to read config file")?;
+        let config = toml::from_str(&contents).context("Failed to parse config")?;
+        let base = config_path.parent().unwrap();
+
+        Vmtest::new(base, config)
+    }
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
     env_logger::init();
-    let contents = fs::read_to_string(&args.config).context("Failed to read config file")?;
-    let config = toml::from_str(&contents).context("Failed to parse config")?;
-    let base = args.config.parent().unwrap();
-    let vmtest = Vmtest::new(base, config)?;
+    let vmtest = config(&args)?;
     let filter = Regex::new(&args.filter).context("Failed to compile regex")?;
     let ui = Ui::new(vmtest);
     let failed = ui.run(&filter);

commit f90448b139ce7bb79783eb609b47b2995fbf19e1

Set correct show_cmd for config file interface
When you use a config file (either automatic discovery of vmtest.toml or
passing --config), show_cmd should be false.

Commit ab25958 ("Support oneliner interface for kernel targets") broke
the show_cmd() helper by setting default path in config() helper.

Fix by checking if kernel one-liner interface is being used.

src/main.rs
@@ -146,7 +146,7 @@ fn config(args: &Args) -> Result<Vmtest> {
 ///
 /// This is useful for one-liner invocations.
 fn show_cmd(args: &Args) -> bool {
-    args.config.is_none()
+    args.kernel.is_some()
 }
 
 fn main() -> Result<()> {
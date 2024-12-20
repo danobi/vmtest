danobi/vmtest

commit 7b29d3a2b73af39e42992e8faa56319701179cdf

[ui][run_one] Return a Result<i32> instead of TargetRcUi
If an error happen when trying to set up/run the VM, return an Error,
otherwise return the return code of the command.

Do not report a non-zero return code from the command as an Error, the command
actually ran, but do log the error to the console to make it more obvious to
the user.

Signed-off-by: Manu Bretelle <chantr4@gmail.com>

src/ui.rs
@@ -2,7 +2,7 @@ use std::cmp::min;
 use std::sync::mpsc::{channel, Receiver};
 use std::thread;
 
-use anyhow::{anyhow, Error};
+use anyhow::{anyhow, bail, Error, Result};
 use console::{strip_ansi_codes, style, truncate_str, Style, Term};
 use regex::Regex;
 
@@ -25,13 +25,6 @@ struct Stage {
     term: Term,
     lines: Vec<String>,
     expand: bool,
-}
-
-struct TargetUiRc {
-    // Return code of the command ran in the VM or None if it never ran.
-    rc: Option<i32>,
-    // Whether or not there was any error running the command, setting up the VM...
-    error: bool,
 }
 
 /// Helper to clear lines depending on whether or not a tty is attached
@@ -167,16 +160,14 @@ impl Ui {
 
     /// UI for a single target. Must be run on its own thread.
     ///
-    /// Returns a TargetUiRc
-    fn target_ui(updates: Receiver<Output>, target: String, show_cmd: bool) -> TargetUiRc {
+    /// Returns an Error if the vm failed to run the command.
+    /// Otherwise, return the return code of the command.
+    fn target_ui(updates: Receiver<Output>, target: String, show_cmd: bool) -> Result<i32> {
         let term = Term::stdout();
         let mut stage = Stage::new(term.clone(), &heading(&target, 1), None);
         let mut stages = 0;
         let mut errors = 0;
-        let mut rc = TargetUiRc {
-            rc: None,
-            error: false,
-        };
+        let mut rc: i32 = 0;
 
         // Main state machine loop
         loop {
@@ -221,13 +212,15 @@ impl Ui {
 
                     match r {
                         Ok(retval) => {
-                            rc.rc = Some(*retval as i32);
+                            rc = *retval as i32;
+                            // Do not increment the error counter here. The VM ran successfully, the error is convyed by
+                            // the command return code.
+                            // Nevertheless, we still want to make it clear to the user by logging to the console.
                             if *retval != 0 {
                                 error_out_stage(
                                     &mut stage,
                                     &anyhow!("Command failed with exit code: {}", retval),
                                 );
-                                errors += 1;
                             }
                         }
                         Err(e) => {
@@ -250,8 +243,10 @@ impl Ui {
             term.write_line("FAILED").expect("Failed to write terminal");
         }
 
-        rc.error = errors != 0;
-        rc
+        if errors != 0 {
+            bail!("Failed to run the target");
+        }
+        Ok(rc)
     }
 
     /// Run all the targets in the provided `vmtest`
@@ -284,21 +279,19 @@ impl Ui {
             // Run a target
             self.vmtest.run_one(idx, sender);
 
-            let rc = ui.join().unwrap();
+            let rc = ui
+                .join()
+                .expect("Failed to join UI thread")
+                // Transform an error into a pre-baked error code that represent a failure to run the VM.
+                .unwrap_or(EX_UNAVAILABLE);
 
-            if !single_cmd {
-                if rc.error {
-                    failed += 1;
-                }
-            } else {
-                match rc.rc {
-                    Some(rc) => {
-                        failed = rc;
-                    }
-                    None => {
-                        failed = EX_UNAVAILABLE;
-                    }
-                }
+            if single_cmd {
+                return rc;
+            }
+
+            failed += match rc {
+                0 => 0,
+                _ => 1,
             }
         }
 

commit 3a5005bdcbe250ea1c6bf83634566da8acad2d76

ui: Mark failed targets as failed for config interface
Commit 7b29d3a ("[ui][run_one] Return a Result<i32> instead of
TargetRcUi") introduced a bug where failed targets under the config
interface were being marked as PASS. This is confusing - all evidence of
failure is hidden from the terminal.

The previous logic was also pretty confusing b/c it used several state
variables with multiple (and invalid) combinations. Simplify all of it
to use an enum so all the states are more obvious.

This closes #103.

src/ui.rs
@@ -165,8 +165,7 @@ impl Ui {
         let term = Term::stdout();
         let mut stage = Stage::new(term.clone(), &heading(&target, 1), None);
         let mut stages = 0;
-        let mut errors = 0;
-        let mut rc: i32 = 0;
+        let mut rc = Some(0);
 
         // Main state machine loop
         loop {
@@ -185,7 +184,7 @@ impl Ui {
                 Output::BootEnd(r) => {
                     if let Err(e) = r {
                         error_out_stage(&mut stage, e);
-                        errors += 1;
+                        rc = None;
                     }
                 }
                 Output::SetupStart => {
@@ -196,7 +195,7 @@ impl Ui {
                 Output::SetupEnd(r) => {
                     if let Err(e) = r {
                         error_out_stage(&mut stage, e);
-                        errors += 1;
+                        rc = None;
                     }
                 }
                 Output::CommandStart => {
@@ -211,20 +210,17 @@ impl Ui {
 
                     match r {
                         Ok(retval) => {
-                            rc = *retval as i32;
-                            // Do not increment the error counter here. The VM ran successfully, the error is convyed by
-                            // the command return code.
-                            // Nevertheless, we still want to make it clear to the user by logging to the console.
                             if *retval != 0 {
                                 error_out_stage(
                                     &mut stage,
                                     &anyhow!("Command failed with exit code: {}", retval),
                                 );
                             }
+                            rc = Some(*retval as i32);
                         }
                         Err(e) => {
                             error_out_stage(&mut stage, e);
-                            errors += 1;
+                            rc = None;
                         }
                     };
                 }
@@ -234,18 +230,24 @@ impl Ui {
         // Force stage cleanup so we can do final fixup if we want
         drop(stage);
 
-        // Only clear target stages if target was successful
-        if errors == 0 && !show_cmd {
-            clear_last_lines(&term, stages);
-            term.write_line("PASS").expect("Failed to write terminal");
-        } else if !show_cmd {
-            term.write_line("FAILED").expect("Failed to write terminal");
+        match rc {
+            Some(0) => {
+                if !show_cmd {
+                    clear_last_lines(&term, stages);
+                    term.write_line("PASS").expect("Failed to write terminal");
+                }
+                Ok(0)
+            }
+            Some(rc) => {
+                if !show_cmd {
+                    term.write_line("FAILED").expect("Failed to write terminal");
+                }
+                Ok(rc)
+            }
+            None => {
+                bail!("Failed to run the target");
+            }
         }
-
-        if errors != 0 {
-            bail!("Failed to run the target");
-        }
-        Ok(rc)
     }
 
     /// Run all the targets in the provided `vmtest`
@@ -255,8 +257,10 @@ impl Ui {
     ///
     /// Note this function is "infallible" b/c on error it will display
     /// the appropriate error message to screen.
+    ///
     /// In one-liner mode, it return the return code of the command, or EX_UNAVAILABLE if there
     /// is an issue that prevents running the command.
+    ///
     /// When multiple targets are ran, it returns how many targets failed.
     pub fn run(self, show_cmd: bool) -> i32 {
         let mut failed = 0;